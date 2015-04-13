11b4ab343fe83145ecd8f46d09ce120b0508aa6d
Natallia La <natallia_lantsuntsevich@epam.com>
2015-04-13 22:53:38 +0300

    Cookbooks, .chef have been added.

---
+++ .chef/solo.rb
@@ -0,0 +1,9 @@
+log_level :debug
+
+file_cache_path "/home/natallia_la/chef-solo/my_cookbook/.chef/"
+
+cookbook_path "/home/natallia_la/chef-solo/my_cookbook/cookbooks/"
+
+json_attribs "/home/natallia_la/chef-solo/my_cookbook/.chef/runlist.json"
+
+
