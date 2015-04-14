9afae2d20a437f7648e5660237ec8f4cd6f28037
df28 <you@example.com>
2015-04-14 23:24:32 +0300

    jboss8 has been added

---
+++ Berksfile
@@ -0,0 +1,4 @@
+source "http://api.berkshelf.com"
+
+cookbook 'java', '~> 1.31.0'
+cookbook 'chef-solo-search', '~> 0.5.1'
---
+++ site-cookbooks/jboss/attributes/default.rb
@@ -0,0 +1,6 @@
+
+
+default['jboss']['jboss_home'] = "/usr/local/jboss/default"
+default['jboss']['version'] = "8.2.0"
+default['jboss']['dl_url'] = "http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz"
+default['jboss']['jboss_user'] = "jboss"
